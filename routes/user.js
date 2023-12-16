5d0e2fa07b65e8d6be5b23ec58ec238ab75f9c4d
ieavci <iavcci123@gmail.com>
2023-12-16 15:41:38 +0300

    16.12.2023
    sendikalar ve sgie veri setleri veritabanına import edildi. sendikalar veritabanı oluşturuldu. veritabanından konfederasyon bilgileri çekilerek sendikalar sayfasında kart içerisine eklendi. modal içeriği veritabanına göre güncellenecek. navbar toggler icon görünememe sorunu düzeltildi.

--- routes/user.js
+++ routes/user.js
@@ -9,11 +9,16 @@ router.use("/haklar", async function (req, res) {
 
     const [tblSosyal] = await db.execute("select * from sosyalsigortalar")
     const [tblCezai] = await db.execute("select * from cezaiyaptirim")
+    const [tblIsverenhaklari] = await db.execute("SELECT m.*, k.kanunbasligi FROM isverenkanunlarimaddeleri m JOIN isverenkanunlari k ON m.kanun_id = k.kanun_id ")
+    const [tblIscihaklari] = await db.execute("SELECT m.*, k.kanunbasligi FROM iscikanunlarimaddeleri m JOIN iscikanunlari k ON m.kanun_id = k.kanun_id ")
+
 
     try {
         res.render("isci-ve-isveren/isci-ve-isveren.ejs", {
             sosyal: tblSosyal[0],
             cezai: tblCezai[0],
+            isveren: tblIsverenhaklari,
+            isci: tblIscihaklari,
         });
 
     } catch (error) {
@@ -23,8 +28,19 @@ router.use("/haklar", async function (req, res) {
 router.use("/sigortasiz", function (req, res) {
     res.render("sigortasiz_isci/sigortasiz-isci.ejs");
 });
-router.use("/sendikalar", function (req, res) {
-    res.render("sendikalar/sendikalar.ejs");
+router.use("/sendikalar", async function (req, res) {
+    const [tblDisk] = await db.execute("select * from `isciatlasi`.`disk sendika`");    
+    const [tblSendikaKonfederasyonlar] = await db.execute("select * from `isciatlasi`.`sendikakonfederasyonlar`");    
+    try {
+        res.render("sendikalar/sendikalar.ejs", {
+            disk: tblDisk,
+            sendikaKonfederasyonlar:tblSendikaKonfederasyonlar,
+        });
+
+    } catch (error) {
+        console.log(error)
+    }
+
 });
 
 
